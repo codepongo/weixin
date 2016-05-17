7a9d033e8833411bc7b9d91d99d98e25e88c6439
unknown <zhuhuotui@gmail.com>
2016-05-17 16:38:08 +0800

    tester

---
+++ tester/wxtester.py
@@ -0,0 +1,30 @@
+#coding:utf8
+import requests
+import time
+url = 'http://127.0.0.1:8080/server'
+msg = '''<xml>
+ <ToUserName><![CDATA[%s]]></ToUserName>
+ <FromUserName><![CDATA[%s]]></FromUserName> 
+ <CreateTime>%d</CreateTime>
+ <MsgType><![CDATA[%s]]></MsgType>
+ <Content><![CDATA[%s]]></Content>
+ <MsgId>%d</MsgId>
+ </xml>'''
+
+def authentication():
+    requests.get(url, params={
+        'signature':'f36141c4c275d2216c9269cf13232308c384f946',
+        'timestamp':'',
+        'nonce':'',
+        'echostr':''
+        })
+
+def post_text(content):
+    r = requests.post(url, data = msg % ('zhuhuotui', 'codepongo', int(time.time()), 'text', content, 1)) 
+    print r.text
+if __name__ == '__main__':
+    #authentication()
+    post_text('北京天气')
+
+
+
