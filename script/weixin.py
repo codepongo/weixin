0f8f55e5b774f3634faccc329c5a5eddc4d85af6
unknown <zhuhuotui@gmail.com>
2016-05-17 16:37:49 +0800

    reply

--- script/weixin.py
+++ script/weixin.py
@@ -4,6 +4,8 @@ import os
 sys.path.append(os.path.dirname(__file__))
 import web
 import hashlib
+import xml.etree.ElementTree
+import time
 try:
     import conf
     token = conf.token
@@ -20,6 +22,7 @@ def check_signature(signature, timestamp, nonce):
     l.sort()
     map(sha1.update, l)
     hashcode = sha1.hexdigest()
+    print hashcode
     if hashcode == signature:
         return True
     else:
@@ -45,9 +48,11 @@ class index():
     def GET(self, query=''):
         return web.template.render(os.path.join(os.path.dirname(__file__), 'templates')).index(None)
 
-class authentication:
+class server:
     def GET(self):
         get_data = web.input()
+        if len(get_data) == 0:
+            raise web.seeother('/index')
         signature = get_data.signature
         timestamp = get_data.timestamp
         nonce = get_data.nonce
@@ -55,8 +60,24 @@ class authentication:
         if check_signature(signature, timestamp, nonce):
             return echo
 
+    def POST(self):
+        pass
+        req = xml.etree.ElementTree.fromstring(web.data())
+        content = req.find("Content").text
+        msgType = req.find("MsgType").text
+        fromUser = req.find("FromUserName").text
+        toUser = req.find("ToUserName").text
+        content = content.encode('utf8')
+        if content.find('天气'):
+            city = content.replace('天气', '')
+            return web.template.render(os.path.join(os.path.dirname(__file__), 'templates')).replytext(fromUser,toUser,int(time.time()),"接收到的文字："+content)
+        else:
+            return web.template.render(os.path.join(os.path.dirname(__file__), 'templates')).replytext(fromUser,toUser,int(time.time()), content)
+
+        
+
 urls = (
-    '/authentication', authentication,
+    '/server', server,
     '/favicon.ico', favicon,
     '/(.*).(jpg)', image,
     '/(.*)', index,
