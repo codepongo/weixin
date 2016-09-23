e7a9a99e67ddcab4b793e7c2ecf4e7867807b729
unknown <zhuhuotui@gmail.com>
2016-09-23 14:19:09 +0800

    fix da le tou

--- script/lottery/lottery_gov_cn.py
+++ script/lottery/lottery_gov_cn.py
@@ -13,27 +13,34 @@ class Parser(HTMLParser.HTMLParser):
         self.flag = None
         HTMLParser.HTMLParser.__init__(self)
     def handle_starttag(self, tag, attrs):
-        if tag == 'td' and len(attrs) == 1 and attrs[0] == ('height', '24'):
+        if tag == 'td' and len(attrs) == 3 and attrs[0] == ('height', '23'):
             self.flag = 'issue'
             return
-        if tag == 'font' and len(attrs) == 1 and attrs[0] == ('class', 'FontRed'):
+        if tag == 'td' and len(attrs) == 3 and attrs[2] == ('class', 'red'):
             self.flag = 'red'
             return
-        if tag == 'font' and len(attrs) == 1 and attrs[0] == ('class', 'FontBlue'):
+        if tag == 'td' and len(attrs) == 4 and attrs[3] == ('class', 'blue'):
             self.flag = 'blue'
             return
-        if tag == 'td' and len(attrs) == 0 and self.item.has_key('blue'):
+        if tag == 'td' and len(attrs) == 2:
             self.flag = 'publish'
             return
+    def handle_endtag(self, tag):
+        if tag == 'tr' and len(self.item.keys()) == 4:
+            self.result.append(self.item)
+
     def handle_data(self, data):
         d = data.strip()
         if self.flag == None or d == '':
             self.flag = None
             return
-        self.item[self.flag] = d
+        if self.flag == 'publish' or self.flag == 'issue':
+            self.item[self.flag] = d
+            return
+        if not self.item.has_key(self.flag):
+            self.item[self.flag] = ''
+        self.item[self.flag] += d + ' '
         self.flag = None
-        if self.item.has_key('publish'):
-            self.result.append(self.item)
 class Found(BaseException):
     pass
 class ParserForLast(Parser):
@@ -44,7 +51,7 @@ class ParserForLast(Parser):
 
 class DLT():
     def last(self):
-        url = 'http://www.lottery.gov.cn/lottery/dlt/History.aspx'
+        url = 'http://www.lottery.gov.cn/historykj/history.jspx?_ltype=dlt'
         html = urllib.urlopen(url).read()
         r = None
         try:
@@ -58,7 +65,6 @@ class DLT():
 
 if __name__ == '__main__':
     r = DLT().last()
-    print r
     numbers = r['red'] + ' + ' + r['blue']
     reply = '%s第%s期%s' % (r['publish'], r['issue'], numbers)
     print reply.decode('utf8')
